b879c3ec1f53c35405b95ac0c7e3eb7838aad468
unknown <skullowner83@gmail.com>
2023-10-27 02:02:08 -0600

    Creating react application

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Header = () => {
+    let WebsiteName = "Servicios Informáticos de Aguascalientes";
+
+  return (
+    <div>
+        <header>
+            <h1></h1>
+
+            <ul>
+                <li>INICIO</li>
+                <li>SERVICIOS</li>
+                <li>PRODUCTOS</li>
+                <li>PRIVACIDAD</li>
+                <li>CONTACTO</li>
+                <li>SOPORTE</li>
+            </ul>
+        </header>
+    </div>
+  )
+}
